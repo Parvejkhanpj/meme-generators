f9061401f91bffe01dba3180f881af2cd3ddc5e3
Parvej khan <97285732+Parvejkhanpj@users.noreply.github.com>
2022-05-07 00:30:47 +0530

    Initial commit

---
+++ src/Component/Meme.js
@@ -0,0 +1,54 @@
+import React from "react";
+import memesData from "../memeData";
+
+export default function Meme() {
+  /**
+   * Challenge: Update our state to save the meme-related
+   * data as an object called `meme`. It should have the
+   * following 3 properties:
+   * topText, bottomText, randomImage.
+   *
+   * The 2 text states can default to empty strings for now,
+   * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+   *
+   * Next, create a new state variable called `allMemeImages`
+   * which will default to `memesData`, which we imported above
+   *
+   * Lastly, update the `getMemeImage` function and the markup
+   * to reflect our newly reformed state object and array in the
+   * correct way.
+   */
+
+  const [meme, setmeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+  });
+
+  //fatect images from data
+
+  const [allMemeImages, setallMemeImages] = React.useState(memesData);
+
+  function getimages() {
+    const memesArray = allMemeImages.data.memes;
+    const random = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[random].url;
+    setmeme((preContant) => ({
+      ...preContant,
+      randomImage: url
+    }));
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button className="form--button" onClick={getimages}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img src={meme.randomImage} className="meme--image" />
+    </main>
+  );
+}
